99198cdd5fbfa9b9b04d9676dc3c2797e2be41d3
aman <aman@DESKTOP-L82JUJH>
2024-07-23 20:21:24 +0530

    Add project files.

---
+++ Logic Building in C#/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic_Building_in_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* print this patten using user input....
+             * 1
+             * 12
+             * 123
+             * 1234
+             * 12345
+             */
+
+            Console.WriteLine("Enter range: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+
+            /*print this patten using user input....
+              
+              1
+              2 2
+              3 3 3
+              4 4 4 4
+              5 5 5 5 5
+             */
+
+
+            Console.WriteLine("Enter the number of Rows: ");
+            int rows1 = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= rows1; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+            }
+
+            /* print this patten using user input....
+             
+               54321
+               5432
+               543
+               54
+               5
+              */
+
+            Console.WriteLine("Enter a row:");
+            int rows2 = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= rows1; i++)
+            {
+                for (int j = rows1; j >= i; j--)
+                {
+                    Console.Write(j +" ");
+                }
+                Console.WriteLine();
+            }
+
+            /* print this patten using user input....
+               1
+               1 0
+               1 0 1
+               1 0 1 0
+               1 0 1 0 1
+             */
+
+            Console.WriteLine("Enter a rows");
+            int rows3 = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= rows2; i++)
+            {
+                for (int k = 1; k <= i; k++)
+                {
+                    if (k % 2 == 0)
+                    {
+                        Console.Write(0 + " ");
+                    }
+                    else
+                    {
+                        Console.Write(1 + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
+
